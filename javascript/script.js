03817b745c6d6df688af48243d2e9be352edbb30
CodyWindram <cody.windram@gmail.com>
2022-04-09 16:43:38 +0200

    Created js classes
    created classes

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+// Create classes
+
+class todoList {
+    constructor(id, task, due){
+        this._id = id;
+        this._task = task;
+        this._due = due;
+        this._delete = false;
+        this._edit = false;
+    }
+}
+
